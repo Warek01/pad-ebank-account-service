dd096b715c81e71b624039419304b79737dcb963
Alexandru Dobrojan <alexandrudobrojan@gmail.com>
2024-10-28 17:54:56 +0200

    Add versioning

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { GrpcOptions, Transport } from '@nestjs/microservices';
 import { WsAdapter } from '@nestjs/platform-ws';
 import { ConfigService } from '@nestjs/config';
-import { INestApplication, Logger, ShutdownSignal } from '@nestjs/common';
+import {
+  INestApplication,
+  Logger,
+  ShutdownSignal,
+  VersioningType,
+} from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Express } from 'express';
 import fs from 'fs/promises';
@@ -25,17 +30,37 @@ async function bootstrap() {
   const grpcPort = config.get('GRPC_PORT');
   const hostname = config.get('HOSTNAME');
 
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: '*',
+    methods: '*',
+  });
+  app.setGlobalPrefix('/api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v',
+    defaultVersion: '1',
+  });
+  app.useWebSocketAdapter(new WsAdapter());
+
   const swaggerConfig = new DocumentBuilder()
-    .setTitle('eBank account service')
+    .setTitle('eBank Account Service')
     .setVersion('1.0.0')
     .addTag('Test', 'Test methods for testing the features')
     .addTag(
       'Health',
       'Healthcheck methods for service discovery and load balancing',
     )
+    .setDescription('Account Microservice HTTP methods')
+    .setBasePath('/api')
     .build();
+
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, swaggerDocument);
+  SwaggerModule.setup('/docs', app, swaggerDocument, {
+    jsonDocumentUrl: '/docs/swagger.json',
+    customSiteTitle: 'Account Service docs',
+    useGlobalPrefix: true,
+  });
 
   const protosPath: string = path.join(__dirname, 'proto');
   const protoFiles: string[] = await fs.readdir(protosPath);
@@ -55,14 +80,8 @@ async function bootstrap() {
     transport: Transport.GRPC,
   };
 
-  app.enableCors({
-    origin: '*',
-    allowedHeaders: '*',
-    methods: '*',
-  });
   app.enableShutdownHooks([ShutdownSignal.SIGTERM, ShutdownSignal.SIGINT]);
   app.connectMicroservice(accountGrpcOptions);
-  app.useWebSocketAdapter(new WsAdapter());
 
   await Promise.all([
     app.startAllMicroservices(),
